a7220f5a83281339e63834bc3c58bcac73bc7e14
Michael Feldstein <msfeldstein@gmail.com>
2021-09-21 21:03:04 -0700

    Make lightning work

--- client/src/animation-components/Lightning.tsx
+++ client/src/animation-components/Lightning.tsx
@@ -1,85 +1,95 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import BaseAnimation from "./Base";
-// import perlin from "./perlin";
-const ROW_SIZE = 16;
-const COL_SIZE = 8;
+import * as core from "@theatre/core";
+import { types as t, ISheetObject } from "@theatre/core";
+import state from "./motion/lightning.json";
+import { perlin2 } from "./perlin2";
 
-const SPEED = 1000;
-const color = "yellow";
-type LightningValues = {
-  lightning: {
-    x: number;
-    y: number;
-    grid: number[][];
-  };
-};
+const X_SIZE = 8;
+const Y_SIZE = 16;
+
+let seed1 = Math.random();
+let seed2 = Math.random();
+
+function fbm(x: number, y: number) {
+  const OCTAVES = 6;
+
+  let normalize_factor = 0.0;
+  let value = 0.0;
+  let scale = 0.5;
+
+  for (let i = 0; i < OCTAVES; i++) {
+    value += perlin2(x, y) * scale;
+    normalize_factor += scale;
+    x *= 2.0;
+    y *= 2.0;
+    scale *= 0.5;
+  }
+  return (value / normalize_factor) * 0.5 + 0.5;
+}
+
 export default function LightningAnimation() {
-  const render = useCallback(
-    (canvas: HTMLCanvasElement, opts, values: LightningValues) => {
-      const grid = values.lightning.grid;
-      if (grid.length === 0) {
-        const rows = Math.ceil(canvas.height / ROW_SIZE);
-        const cols = Math.ceil(canvas.width / COL_SIZE);
-        for (let y = 0; y < rows; y++) {
-          const row = [];
-          for (let x = 0; x < cols; x++) {
-            // row.push(random(x * 23.234 + y * 392.23) / 2);
-            row.push(Math.sin(Math.cos(x * 23 + y * 1) * 2));
-          }
-          grid.push(row);
-        }
-      }
-      values.lightning.x += 2;
-      values.lightning.y += 4;
-      const ctx = canvas.getContext("2d")!;
-      let t = opts.time / SPEED;
-      const phase1 = t < 1;
-      for (let x = 0; x < canvas.width; x += COL_SIZE) {
-        for (let y = 0; y < canvas.height; y += ROW_SIZE) {
-          const iy = Math.floor(y / ROW_SIZE);
-          const ix = Math.floor(x / COL_SIZE);
-          if (phase1) {
-            if (
-              x > values.lightning.x &&
-              x < values.lightning.x + COL_SIZE &&
-              y > values.lightning.y &&
-              y < values.lightning.y + ROW_SIZE
-            ) {
-              grid[iy][ix] = 1;
-            }
-            ctx.fillStyle = grid[iy][ix] === 1 ? color : "transparent";
-          } else {
-            if (grid[iy][ix] === 1) {
-              ctx.fillStyle = "black";
-            } else {
-              ctx.fillStyle = color;
-            }
-            ctx.globalAlpha = Math.max(2 - t, 0) * grid[iy][ix];
-          }
-          ctx.fillRect(x, y, COL_SIZE, ROW_SIZE);
-        }
-      }
+  const [values, setValues] = useState<ISheetObject<{
+    strike1: number;
+    strike2: number;
+    whiteout: number;
+  }> | null>(null);
 
-      // grid.forEach((row, rowIndex) => {
-      //   row.forEach((val, colIndex) => {
-      //     ctx.globalAlpha = val;
-      //     ctx.fillRect(
-      //       colIndex * COL_SIZE,
-      //       rowIndex * ROW_SIZE,
-      //       COL_SIZE,
-      //       ROW_SIZE
-      //     );
-      //   });
-      // });
+  useEffect(() => {
+    const proj = core.getProject("lightning", { state });
+    const sheet = proj.sheet("scene");
+
+    const lightningProps = sheet.object("Lightning", {
+      strike1: t.number(0, {
+        range: [0, 1],
+      }),
+      strike2: t.number(0, {
+        range: [0, 1],
+      }),
+      whiteout: t.number(0, {
+        range: [0, 1],
+      }),
+    });
+
+    proj.ready.then(() => {
+      sheet.sequence.play({ range: [0, 2], rate: 0.7 });
+    });
+    setValues(lightningProps);
+  }, []);
+  let frameSkip = 0;
+  const render = useCallback(
+    (canvas: HTMLCanvasElement, opts) => {
+      if (!values) return;
+      const context = canvas.getContext("2d")!;
+      const { width, height } = canvas;
+      const { strike1, strike2, whiteout } = values.value;
+
+      function strike(x: number, y: number, v: number, seed: number) {
+        const yNorm = y / height;
+        const fbmX = (x / 12 / X_SIZE) * 0.35;
+        const fbmY = (y / 12 / Y_SIZE) * 0.35;
+        const fbmNoise =
+          1.0 -
+          3 * Math.pow(Math.abs(fbm(fbmX + seed, fbmY / 3 + seed) - 0.5), 0.5);
+        const fadeValue = Math.max(0, 1 - Math.abs(v - 1));
+        let value = fbmNoise * Math.max(0, 1 - yNorm * 1 - 1 + fadeValue * 2);
+        value = value * Math.max(0, 1 - yNorm * 2 - 1 + 1 * 2);
+        const percent = value * 100;
+        return Math.max(percent, 0);
+      }
+      for (var x = 0; x < width; x += X_SIZE) {
+        for (var y = 0; y < height; y += Y_SIZE) {
+          const s1 = strike(x, y, strike1, seed1);
+          const s2 = strike(x, y, strike2, seed2);
+          const w = Math.max(whiteout * 100 - strike(x, y, 1, seed2) / 2, 0);
+          let v = Math.max(Math.max(s1, s2), w);
+          v = Math.floor(v * 8) / 8;
+          context.fillStyle = `hsla(60.0, 100%, ${v}%, ${v / 100})`;
+          context.fillRect(x, y, X_SIZE, Y_SIZE);
+        }
+      }
     },
-    []
+    [values]
   );
-  const initialValues: LightningValues = {
-    lightning: {
-      x: 8,
-      y: 8,
-      grid: [],
-    },
-  };
-  return <BaseAnimation render={render} initialValues={initialValues} />;
+  return <BaseAnimation render={render} />;
 }
